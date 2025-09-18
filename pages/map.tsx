a5399591cae112a8188e7f44ef06af5bac5655ad
Artimuz <alanrodroogs@gmail.com>
2025-09-17 21:42:36 -0300

    translations

--- pages/map.tsx
+++ pages/map.tsx
@@ -222,7 +222,7 @@ export default function MapPage() {
                   onClick={() => setActiveSlot(null)}
                   className="px-4 py-2 bg-black rounded hover:bg-gray-600"
                 >
-                  Fechar
+                  Close
                 </button>
               </div>
             </div>
