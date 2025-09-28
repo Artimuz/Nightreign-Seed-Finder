77d76dfb25947f4bb6368888b42e69b075458d18
Artimuz <alanrodroogs@gmail.com>
2025-09-28 04:24:59 -0300

    cache fix

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/Images/:all*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
